b362b370d92798df9688657d83b1981a073bb843
camerash <esmond9981127@gmail.com>
2019-02-14 16:20:38 +0800

    wip: progress on task 2

--- Assignment1/cs231n/classifiers/linear_svm.py
+++ Assignment1/cs231n/classifiers/linear_svm.py
@@ -27,21 +27,38 @@ def svm_loss_naive(W, X, y, reg):
   num_train = X.shape[0]
   loss = 0.0
   for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
+    scores = X[i].dot(W) # Score for the respective data
+    correct_class_score = scores[y[i]] # Score for the correct class
     for j in xrange(num_classes):
       if j == y[i]:
+        # Trained label same with the class we are computing the gradient for
+        # Skipping
         continue
+
       margin = scores[j] - correct_class_score + 1 # note delta = 1
+      # If the current class has a better score then the correct one, normalize
       if margin > 0:
+        trained_label = y[i] # Get the class label of the corresponding data
+
+        # Add [Xi1, Xi2, ..., XiD], that is the data array of the i-th data
+        # into
+        # the array of weight of dimension (gradient) of the respective class
+        dW[:,j] += X[i,:]
+
+        # Substract [Xi1, Xi2, ..., XiD], that is the data array of the i-th data
+        # from
+        # the array of weight of dimension (gradient) of the trained class
+        dW[:,trained_label] -= X[i,:]
         loss += margin
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train # Same with the gradient array
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += reg * W  # Same with the gradient array
 
   #############################################################################
   # TODO:                                                                     #
@@ -51,7 +68,6 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
 
   return loss, dW
 

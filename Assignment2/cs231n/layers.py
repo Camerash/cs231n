b82f6e3563a25569a0c177f59190a61e1b9087a7
Esmond <esmond9981127@gmail.com>
2019-03-05 23:26:57 +0800

    feat: finish affine passes

--- Assignment2/cs231n/layers.py
+++ Assignment2/cs231n/layers.py
@@ -25,7 +25,10 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
-    pass
+    out_shape = x.shape[:1]
+    out_shape = out_shape + (-1,) # numpy reshape by auto redimension
+    out = x.reshape(out_shape)
+    out = np.matmul(out, w) + b
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -53,7 +56,12 @@ def affine_backward(dout, cache):
     ###########################################################################
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
-    pass
+    x_orig_shape = x.shape
+    x_shape = x_orig_shape[:1] + (-1,) # numpy reshape by auto redimension
+    x = x.reshape(x_shape)
+    dx = np.matmul(dout, w.T).reshape(x_orig_shape) # Reshape into x's original shape
+    dw = np.matmul(x.T, dout)
+    db = np.sum(dout, axis=0)
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################

f4c4d87d1b59ef4ba8de50115eb0e26922131eaa
Esmond <esmond9981127@gmail.com>
2019-03-12 17:09:20 +0800

    feat: finish cnn part 1

--- Assignment2/cs231n/classifiers/cnn.py
+++ Assignment2/cs231n/classifiers/cnn.py
@@ -48,7 +48,14 @@ class ThreeLayerConvNet(object):
         # hidden affine layer, and keys 'W3' and 'b3' for the weights and biases   #
         # of the output affine layer.                                              #
         ############################################################################
-        pass
+        self.params['W1'] = np.random.normal(scale=weight_scale, size=(num_filters, input_dim[0], filter_size, filter_size))
+        self.params['b1'] = np.zeros(num_filters)
+
+        self.params['W2'] = np.random.normal(scale=weight_scale, size=(input_dim[1]*input_dim[2]*num_filters//4, hidden_dim))
+        self.params['b2'] = np.zeros(hidden_dim)
+
+        self.params['W3'] = np.random.normal(scale=weight_scale, size=(hidden_dim, num_classes))
+        self.params['b3'] = np.zeros(num_classes)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -80,7 +87,9 @@ class ThreeLayerConvNet(object):
         # computing the class scores for X and storing them in the scores          #
         # variable.                                                                #
         ############################################################################
-        pass
+        out1, cache1 = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
+        out2, cache2 = affine_relu_forward(out1, W2, b2)
+        scores, cache3 = affine_forward(out2, W3, b3)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -95,7 +104,16 @@ class ThreeLayerConvNet(object):
         # data loss using softmax, and make sure that grads[k] holds the gradients #
         # for self.params[k]. Don't forget to add L2 regularization!               #
         ############################################################################
-        pass
+        loss, dScores = softmax_loss(scores, y)
+        loss += 0.5 * self.reg * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + (np.sum(np.square(W3))))
+        
+        dAffine3, grads['W3'], grads['b3'] = affine_backward(dScores, cache3)
+        dAffine2, grads['W2'], grads['b2'] = affine_relu_backward(dAffine3, cache2)
+        _, grads['W1'], grads['b1'] = conv_relu_pool_backward(dAffine2, cache1)
+
+        grads['W3'] += self.reg * W3
+        grads['W2'] += self.reg * W2
+        grads['W1'] += self.reg * W1
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
--- Assignment2/cs231n/optim.py
+++ Assignment2/cs231n/optim.py
@@ -65,6 +65,7 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
+    # http://cs231n.github.io/neural-networks-3/#ada
     v = config['momentum'] * v - config['learning_rate'] * dw # integrate velocity
     config['velocity'] = v
     next_w = w + v # integrate position
@@ -101,6 +102,7 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
+    # http://cs231n.github.io/neural-networks-3/#ada
     config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
     next_x = x + (-config['learning_rate']) * dx / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
@@ -139,9 +141,13 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
+    # http://cs231n.github.io/neural-networks-3/#ada
+    config['t'] += 1
     config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
     config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * dx**2
-    next_x = x + (-config['learning_rate']) * config['m'] / (np.sqrt(config['v']) + config['epsilon'])
+    config['mt'] = config['m'] / (1-config['beta1']**config['t'])
+    config['vt'] = config['v'] / (1-config['beta2']**config['t'])
+    next_x = x + (-config['learning_rate']) * config['mt'] / (np.sqrt(config['vt']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
